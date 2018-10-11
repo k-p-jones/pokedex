41e8be8ad0a2519295896ccd2f441e4c7322b2aa
kenneth peter jones <kenjones620@yahoo.co.uk>
2018-10-11 21:47:15 +0100

    add support for fetching pokemon json by ID or NAME

--- app.rb
+++ app.rb
@@ -2,7 +2,37 @@ require 'sinatra/base'
 require 'json'
 
 class App < Sinatra::Base
-  get '/' do
-    'Hello World'
+  get '/pokemon' do
+    pokemon_index.to_json
+  end
+
+  # Key can be a pokemon name or id
+  get '/pokemon/:key' do
+    if params[:key].match(/(\d+)/)
+      target = pokemon_index.find { |pokemon| pokemon['id'] == params[:key].match(/(\d+)/)[1] }
+      return pokemon_not_found(params[:key]) unless target
+      File.read("api/v2/pokemon/#{params[:key].match(/(\d+)/)[1]}/index.json")
+    else
+      target = pokemon_index.find { |pokemon| pokemon['name'] == params[:key] }
+      return pokemon_not_found(params[:key]) unless target
+      File.read(File.join(target['url'].sub("/", ''), 'index.json'))
+    end
+  end
+
+  private
+
+  def pokemon_not_found(key)
+    [404, {}, {error: "#{key} didn't match any Pokemon"}.to_json]
+  end
+
+  def pokemon_index
+    @pokemon_index ||= generate_index
+  end
+
+  def generate_index
+    data = JSON.parse(File.read('api/v2/pokemon/index.json'))
+    data['results'].each do |pokemon|
+      pokemon['id'] = pokemon['url'].match(/\/api\/v2\/pokemon\/(\d+)/)[1]
+    end
   end
 end
